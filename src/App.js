107579a15fbd5a8b33b57ba1c7a5bfd4a4b3e680
IlhanKurultay <ilhan.kurultay@student.ehb.be>
2023-11-14 16:03:25 +0100

    Created react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="Desktop">
+      <div className="Gradient_radial"></div>
+      <h1>Hallo</h1>
+    </div>
+  );
+}
+
+export default App;
